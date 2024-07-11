46ed6e38f86fb0869f30f8b90c60fed034c3ce04
Muhammad Abdullah <124899260+Pkabdullah@users.noreply.github.com>
2024-07-11 20:09:14 +0500

    Update index.js

--- src/app/index.js
+++ src/app/index.js
@@ -1,39 +1,32 @@
-
-import { Delete, DeleteIcon, Pen, PlusSquare } from 'lucide-react';
-import React from 'react'
+"use client"
+import { Pen, } from 'lucide-react';
+import React, { useEffect } from 'react'
 import Link from 'next/link';
-import { Button } from '@/components/ui/button';
 import Deletebtn from '@/components/ui/delete';
+import { useState } from 'react';
 
 
+export default function Data() {
 
+    const [pro, setpro] = useState([])
+    async function Getdata() {
+        let data = await fetch("./api")
 
+        data = await data.json();
+        setpro(data.result)
+    }
 
-async function getdata() {
-    let data = await fetch(`../api/products`,{
-        cache:'no-store'
-    })
-    data = await data.json();
+    useEffect(() => {
 
-     return data.result
-}
+        Getdata();
 
-export default async function Data() {
-
-   let pro = await getdata()
+    }, [])
     return (
         <>
-            <div className="">
-                <div className="flex justify-end mr-10 mt-[-60px] ">
-                    <Link href={"./AddData"}>
-                        <Button>Add Product</Button>
-                    </Link>
-                </div>
-            </div>
-            
-        <div className="grid grid-cols-1 ">
-                {pro.map((item) =>(
-                    <div key={item.i_d}>    
+
+            <div className="grid grid-cols-1 ">
+                {pro.map((item) => (
+                    <div key={item.i_d}>
                         <div className="">
                             <div className=" grid grid-cols-1 border-2 border-black w-[40%]  h-24 ml-[4%] mt-16 bg-yellow-200 p-8 rounded-lg shadow-lg ">
                                 <div className="flex flex-col mt-[-20px] ">
@@ -42,30 +35,22 @@ export default async function Data() {
 
                                     <div className="flex justify-end mt-[-30px] cursor-pointer gap-5">
                                         <Link href={""}>
-                                            <Deletebtn id = {item._id}/> 
-                                           
+                                            <Deletebtn id={item._id} />
                                         </Link>
-                                        <Link href={"./EditProduct/"+item._id}>
+                                        <Link href={"./EditProduct/" + item._id}>
                                             <Pen />
                                         </Link>
                                     </div>
                                 </div>
                             </div>
                         </div>
-
-
                     </div>
 
-                    
                 ))
-            }
+                }
 
-                   </div>
-            
-         
-
+            </div>
         </>
-
     )
 }
 
