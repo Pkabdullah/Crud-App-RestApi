16f89ba6f09dc4792456eef7b60e2957540767ff
Muhammad Abdullah <124899260+Pkabdullah@users.noreply.github.com>
2024-07-11 18:12:14 +0500

    Update Header.jsx

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -5,7 +5,18 @@ export default function Header() {
             <div className="bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-lg  shadow-lg w-full  h-24 rounded-b-xl ">
                 <div className="flex justify-center ">
                     <h1 className="mt-9 text-2xl font-bold">Todo List</h1>
+
+                    
                 </div>
+
+                  <div className="">
+                <div className="flex justify-end mr-10 mt-[-60px] ">
+                    <Link href={"./AddData"}>
+                        <Button>Add Product</Button>
+                    </Link>
+                </div>
+            </div>
+
             </div>
 )
 }
